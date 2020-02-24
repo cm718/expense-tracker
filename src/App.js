b2d27f9e0e7b2b39cb67e8cac921889ecc665ba5
cm718 <cm718@icloud.com>
2020-02-24 00:51:04 -0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from "react";
 import "./styles.css";
 import Header from "./components/Header";
+import Balance from "./components/Balance";
+import { incomeExpenses } from './components/incomeExpenses'
 
 export default function App() {
   return (
     <div className="App">
       <Header />
+      <div className="container">
+        <Balance />
+      </div>
+      <incomeExpenses />
     </div>
   );
 }
+
