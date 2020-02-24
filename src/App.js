8d511d63c43320130f372d0ed48b3af30f619eb8
cm718 <cm718@icloud.com>
2020-02-24 01:12:19 -0600

    Added TransactionList and AddTransaction to main app

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Header from "./components/Header";
 import Balance from "./components/Balance";
 import IncomeExpense from './components/IncomeExpense'
 import {TransactionList} from './components/TransactionList'
+import {AddTransaction} from './components/AddTransaction'
 
 export default function App() {
   return (
@@ -12,6 +13,8 @@ export default function App() {
       <div className="container">
         <Balance />
         <IncomeExpense />
+        <TransactionList />
+        <AddTransaction />
       </div>
     </div>
   );
