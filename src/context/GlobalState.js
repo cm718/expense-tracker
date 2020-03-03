201cd3237dc2a08a85c0f556e62470f9ba634523
cm718 <cm718@icloud.com>
2020-03-02 23:28:57 -0600

    Formatted the document

--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -1,5 +1,5 @@
 import React, { createContext, useReducer } from "react";
-import AppReducer from './AppReducer'
+import AppReducer from "./AppReducer";
 
 const initialState = {
   transactions: [
@@ -11,29 +11,35 @@ const initialState = {
 };
 
 // Global Context
-export const GlobalContext = createContext(initialState)
+export const GlobalContext = createContext(initialState);
 
 // Global Provider
 export const GlobalProvider = ({ children }) => {
-    const [state, dispatch] = useReducer(AppReducer, initialState)
+  const [state, dispatch] = useReducer(AppReducer, initialState);
 
-    // Actions
-    const deleteTransaction = id => {
-      dispatch({
-        type: 'DELETE_TRANSACTION',
-        payload: id
-      })
-    }
-    const addTransaction = transaction => {
-      dispatch({
-        type: 'ADD_TRANSACTION',
-        payload: transaction
-      })
-    }
+  // Actions
+  const deleteTransaction = id => {
+    dispatch({
+      type: "DELETE_TRANSACTION",
+      payload: id
+    });
+  };
+  const addTransaction = transaction => {
+    dispatch({
+      type: "ADD_TRANSACTION",
+      payload: transaction
+    });
+  };
 
-    return(
-        <GlobalContext.Provider value={{transactions: state.transactions, deleteTransaction, addTransaction}}>
-            { children }
-        </GlobalContext.Provider>
-    )
-}
+  return (
+    <GlobalContext.Provider
+      value={{
+        transactions: state.transactions,
+        deleteTransaction,
+        addTransaction
+      }}
+    >
+      {children}
+    </GlobalContext.Provider>
+  );
+};
